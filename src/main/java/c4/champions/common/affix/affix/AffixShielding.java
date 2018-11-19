17bb43f7b254771235c7d0e6b77468a665c48d39
TheIllusiveC4 <itwasmec4@gmail.com>
2018-11-19 02:36:47 -0800

    change shielding layer to particles

--- src/main/java/c4/champions/common/affix/affix/AffixShielding.java
+++ src/main/java/c4/champions/common/affix/affix/AffixShielding.java
@@ -7,6 +7,7 @@ import c4.champions.common.capability.IChampionship;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 
 public class AffixShielding extends AffixBase {
@@ -17,15 +18,18 @@ public class AffixShielding extends AffixBase {
 
     @Override
     public void onUpdate(EntityLiving entity, IChampionship cap) {
+        AffixNBT.Boolean shielding = AffixNBT.getData(cap, getIdentifier(), AffixNBT.Boolean.class);
 
-        if (!entity.world.isRemote && entity.ticksExisted % 40 == 0) {
-            AffixNBT.Boolean shielding = AffixNBT.getData(cap, getIdentifier(), AffixNBT.Boolean.class);
-            float chance = shielding.mode ? 0.8f : 0.3f;
+        if (!entity.world.isRemote) {
 
-            if (entity.getRNG().nextFloat() < chance) {
+            if (entity.ticksExisted % 40 == 0 && entity.getRNG().nextFloat() < 0.5f) {
                 shielding.mode = !shielding.mode;
                 shielding.saveData(entity);
             }
+        } else if (shielding.mode) {
+            entity.world.spawnParticle(EnumParticleTypes.SPELL_MOB, entity.posX + (entity.getRNG().nextDouble() - 0.5D)
+                            * (double) entity.width, entity.posY + entity.getRNG().nextDouble() * (double) entity.height,
+                    entity.posZ + (entity.getRNG().nextDouble() - 0.5D) * (double) entity.width, 1, 1, 1);
         }
     }
 
--- src/main/java/c4/champions/proxy/ClientProxy.java
+++ src/main/java/c4/champions/proxy/ClientProxy.java
@@ -3,24 +3,17 @@ package c4.champions.proxy;
 import c4.champions.Champions;
 import c4.champions.client.EventHandlerClient;
 import c4.champions.client.fx.ParticleRank;
-import c4.champions.client.layer.LayerShielding;
 import c4.champions.client.renderer.RenderArcticSpark;
 import c4.champions.common.entity.EntityArcticSpark;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.model.ModelBase;
-import net.minecraft.client.renderer.entity.Render;
-import net.minecraft.client.renderer.entity.RenderLiving;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
-import java.util.Map;
 import java.util.Random;
 
 @Mod.EventBusSubscriber(modid = Champions.MODID, value = Side.CLIENT)
@@ -36,21 +29,6 @@ public class ClientProxy implements IProxy {
     @Override
     public void init(FMLInitializationEvent evt) {
         MinecraftForge.EVENT_BUS.register(new EventHandlerClient());
-    }
-
-    @Override
-    public void postInit(FMLPostInitializationEvent evt) {
-        Map<Class <? extends Entity> , Render<? extends Entity >> renderMap = Minecraft.getMinecraft().getRenderManager()
-                .entityRenderMap;
-
-        for (Render<? extends Entity> render : renderMap.values()) {
-
-            if (render instanceof RenderLiving) {
-                RenderLiving livingRender = (RenderLiving)render;
-                ModelBase model = livingRender.getMainModel();
-                livingRender.addLayer(new LayerShielding(livingRender, model));
-            }
-        }
     }
 
     @Override
