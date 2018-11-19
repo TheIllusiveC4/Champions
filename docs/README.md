dc3b685106ff168a44d988a5541b7a45b1447f01
TheIllusiveC4 <itwasmec4@gmail.com>
2018-11-19 04:36:30 -0800

    some cleanup

---
+++ docs/README.md
@@ -0,0 +1,5 @@
+# Champions [![](http://cf.way2muchnoise.eu/versions/champions.svg)](https://minecraft.curseforge.com/projects/champions) [![](http://cf.way2muchnoise.eu/short_champions_downloads.svg)](https://minecraft.curseforge.com/projects/champions/files) [![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)
+
+Champions is a mod that adds elite mobs of different rarities, with enhanced stats, extra abilities, and extra loot. These champions can be spawned for any hostile mob in the game. Many aspects of this mod can be configured with either the config file or the generated json files, allowing users to adjust the difficulty and diversity of this mod to suit their particular playstyle.
+
+
--- src/main/java/c4/champions/Champions.java
+++ src/main/java/c4/champions/Champions.java
@@ -34,23 +34,26 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.storage.loot.LootTableList;
 import net.minecraft.world.storage.loot.properties.EntityPropertyManager;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.event.*;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = Champions.MODID, name = Champions.NAME, version = Champions.VERSION)
+@Mod(   modid = Champions.MODID,
+        name = Champions.NAME,
+        version = "@VERSION@",
+        dependencies = "required-after:forge@[14.23.5.2768,)",
+        acceptedMinecraftVersions = "[1.12, 1.13)",
+        certificateFingerprint = "@FINGERPRINT@")
 public class Champions
 {
     public static final String MODID = "champions";
     public static final String NAME = "Champions";
-    public static final String VERSION = "1.0";
 
-    private static final boolean DEBUG = true;
+    private static final boolean DEBUG = false;
 
     public static Logger logger;
 
@@ -81,6 +84,11 @@ public class Champions
         AffixFilterManager.readAffixFiltersFromJson();
         ChampionHelper.parseDimensionConfigs();
         proxy.postInit(evt);
+    }
+
+    @EventHandler
+    public void onFingerPrintViolation(FMLFingerprintViolationEvent evt) {
+        FMLLog.log.log(Level.ERROR, "Invalid fingerprint detected! The file " + evt.getSource().getName() + " may have been tampered with. This version will NOT be supported by the author!");
     }
 
     @EventHandler
