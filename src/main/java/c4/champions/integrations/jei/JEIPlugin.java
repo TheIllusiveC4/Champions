f3f4f95d93c1db6f92abd33f22cd26a72bab8683
TheIllusiveC4 <itwasmec4@gmail.com>
2019-01-18 22:01:16 -0800

    blacklist champion egg from jei

---
+++ src/main/java/c4/champions/integrations/jei/JEIPlugin.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2018-2019  C4
+ *
+ * This file is part of Champions, a mod made for Minecraft.
+ *
+ * Champions is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Champions is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Champions.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package c4.champions.integrations.jei;
+
+import c4.champions.common.init.ChampionsRegistry;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.IModRegistry;
+import net.minecraft.item.ItemStack;
+
+@mezz.jei.api.JEIPlugin
+public class JEIPlugin implements IModPlugin {
+
+    @Override
+    public void register(IModRegistry registry) {
+        registry.getJeiHelpers().getIngredientBlacklist().addIngredientToBlacklist(new ItemStack(ChampionsRegistry.championEgg));
+    }
+}
