3ae394373405e1f4d4aa122d51e1f584c8137227
TheIllusiveC4 <29991504+TheIllusiveC4@users.noreply.github.com>
2021-11-09 16:24:18 -0800

    better mod compatibility with boss bar rendering, closes #96

--- src/main/java/top/theillusivec4/champions/client/ClientEventHandler.java
+++ src/main/java/top/theillusivec4/champions/client/ClientEventHandler.java
@@ -1,10 +1,12 @@
 package top.theillusivec4.champions.client;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
 import java.util.Optional;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.LivingEntity;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import top.theillusivec4.champions.client.util.HUDHelper;
 import top.theillusivec4.champions.client.util.MouseHelper;
@@ -16,11 +18,17 @@ public class ClientEventHandler {
   public void renderChampionHealth(RenderGameOverlayEvent.Pre evt) {
 
     if (evt.getType() == ElementType.BOSSHEALTH && ChampionsConfig.showHud) {
-      Optional<LivingEntity> livingEntity = MouseHelper
-          .getMouseOverChampion(Minecraft.getInstance(), evt.getPartialTicks());
+      Minecraft mc = Minecraft.getInstance();
+      Optional<LivingEntity> livingEntity =
+          MouseHelper.getMouseOverChampion(mc, evt.getPartialTicks());
       livingEntity.ifPresent(entity -> {
-        if (HUDHelper.renderHealthBar(evt.getMatrixStack(), entity)) {
+        MatrixStack matrixStack = evt.getMatrixStack();
+
+        if (HUDHelper.renderHealthBar(matrixStack, entity)) {
           evt.setCanceled(true);
+          MinecraftForge.EVENT_BUS.post(new RenderGameOverlayEvent.Post(matrixStack,
+              new RenderGameOverlayEvent(matrixStack, evt.getPartialTicks(), mc.getMainWindow()),
+              ElementType.BOSSHEALTH));
         }
       });
     }
