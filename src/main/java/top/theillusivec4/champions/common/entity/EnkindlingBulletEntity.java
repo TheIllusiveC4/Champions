a7b86f74b37c60d4b3c25934653e600b956c3ceb
TheIllusiveC4 <29991504+TheIllusiveC4@users.noreply.github.com>
2022-04-08 21:45:24 -0700

    fix enkindling death message, closes #112

--- src/main/java/top/theillusivec4/champions/common/entity/EnkindlingBulletEntity.java
+++ src/main/java/top/theillusivec4/champions/common/entity/EnkindlingBulletEntity.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.particles.IParticleData;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.Direction.Axis;
+import net.minecraft.util.EntityDamageSource;
 import net.minecraft.util.IndirectEntityDamageSource;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
@@ -19,24 +20,26 @@ public class EnkindlingBulletEntity extends AbstractBulletEntity {
 
   @OnlyIn(Dist.CLIENT)
   public EnkindlingBulletEntity(World worldIn, double x, double y, double z, double motionXIn,
-      double motionYIn, double motionZIn) {
+                                double motionYIn, double motionZIn) {
     super(ChampionsRegistry.ENKINDLING_BULLET, worldIn, x, y, z, motionXIn, motionYIn, motionZIn);
   }
 
   public EnkindlingBulletEntity(World worldIn, LivingEntity ownerIn, Entity targetIn,
-      Axis direction) {
+                                Axis direction) {
     super(ChampionsRegistry.ENKINDLING_BULLET, worldIn, ownerIn, targetIn, direction);
   }
 
   @Override
   protected void bulletEffect(Entity target) {
 
-    if (target instanceof LivingEntity) {
+    if (this.func_234616_v_() != null) {
+      target.attackEntityFrom(new IndirectEntityDamageSource("cinderBullet.indirect", this,
+          this.func_234616_v_()).setFireDamage().setMagicDamage(), 1111);
+    } else {
       target.attackEntityFrom(
-          new IndirectEntityDamageSource("cinderBullet", this.getOwner(), this).setFireDamage()
-              .setMagicDamage(), 1);
-      target.setFire(8);
+          new EntityDamageSource("cinderBullet", this).setFireDamage().setMagicDamage(), 1111);
     }
+    target.setFire(8);
   }
 
   @Override
